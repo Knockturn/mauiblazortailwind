7bbb10f7e00a45f55f4a69b639bc04084a37cb99
P <pkb@solar.dk>
2024-03-06 14:35:10 +0100

    init commit

---
+++ MauiBlazorTailwindApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorTailwindApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MauiBlazorTailwindApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorTailwindApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
